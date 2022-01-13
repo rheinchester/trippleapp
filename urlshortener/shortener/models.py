c7192e56f8daadaff5cbe3a4bb63358d83e1eafe
rheinchester <jaycobokoro@gmail.com>
2022-01-13 14:33:20 +0000

    Makemigrations(shortener)

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class InputUrl(models.Model):
+    link = models.CharField(max_length=100000)
+    uuid = models.CharField(max_length=10)
+
