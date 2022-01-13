49a3eb7e4cab68249e4687e8738aae1db5efc508
rheinchester <jaycobokoro@gmail.com>
2022-01-13 14:16:26 +0000

    Landing page of shortener working perfectly

--- urlshortener/urlshortener/urls.py
+++ urlshortener/urlshortener/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = [#url mapping for project
     path('admin/', admin.site.urls),
+    path('', include('shortener.urls')),
+    # path('hello', 'helloapp.urls')
 ]
